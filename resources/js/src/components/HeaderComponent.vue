508409f371d58d2c9401f99ce4931f4fe8b93bae
d.kononchenko <kononchenko.dima@yandex.ru>
2024-01-26 17:20:13 +0500

    init8

--- resources/js/src/components/HeaderComponent.vue
+++ resources/js/src/components/HeaderComponent.vue
@@ -40,7 +40,7 @@ export default defineComponent({
   mounted() {
     console.log('hello');
     axios
-        .get('http://localhost:4006/getAuthStatus')
+        .get('/getAuthStatus')
         .then(response =>
         {
           this.userInfo = response.data
