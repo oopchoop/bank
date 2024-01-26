540d0a156e80d899e7c780b23ffb5e7a1e6f64e3
d.kononchenko <kononchenko.dima@yandex.ru>
2024-01-26 06:20:25 +0500

    intit3

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\CardsController;
+use App\Http\Controllers\CardsTypeController;
+use App\Http\Controllers\CreditsController;
+use App\Http\Controllers\CreditsTypeController;
+use App\Http\Controllers\CsrfController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UsersCardsController;
+use App\Http\Controllers\UsersCreditsController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,28 +18,50 @@ use Inertia\Inertia;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::get('/admin', function ()
+{
+   if(empty(Auth::user())) return redirect('/auth');
+   else if(Auth::user()->isAdmin) return redirect('/');
+   else return redirect('/admin');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::post('/registration', [UserController::class, 'reg_post']);
+Route::post('/authorization', [UserController::class, 'auth_post']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/getAuthStatus', [UserController::class, 'getAuthStatus']);
+Route::get('/logout', [UserController::class, 'logOut']);
 
-require __DIR__.'/auth.php';
+Route::get('/getCredits', [CreditsController::class, 'getCredits']);
+Route::get('/getCards', [CardsController::class, 'getCards']);
+
+Route::get('/getUserCards', [UsersCardsController::class, 'getUserCards']);
+Route::get('/getUserCredits', [UsersCreditsController::class, 'getCreditsToUser']);
+
+Route::get('/getCardsFromId/{id}', [CardsController::class, 'getCardsFromId']);
+Route::get('/addCardToUser/{id}', [UsersCardsController::class, 'addCardToUser']);
+
+Route::get('/getCreditFromId/{id}', [CreditsController::class, 'getCreditFromId']);
+Route::post('/addCredit', [UsersCreditsController::class, 'addStroke']);
+
+Route::get('/getPost/{id}', [NewsController::class, 'getNewsFromId']);
+Route::get('/getNews', [NewsController::class, 'getNews']);
+
+Route::get('/getCreditTypes', [CreditsTypeController::class, 'getCreditTypes']);
+Route::get('/getCardsType', array(CardsTypeController::class, 'getCardsType'));
+
+Route::get('/deleteCredit/{id}', [CreditsController::class, 'deleteCredit']);
+
+Route::post('/updateCredit', [CreditsController::class, 'updateCredit']);
+Route::post('/addNewCredit', [CreditsController::class, 'addNewCredit']);
+
+Route::get('/deleteCard/{id}', [CardsController::class, 'deleteCard']);
+Route::post('/addNewCard', [CardsController::class, 'addNewCard']);
+Route::post('/updateCard', [CardsController::class, 'updateCard']);
+
+Route::get('{any?}', fn() => view('app'))
+    ->where('any', '.*');
